47cf3e61fecb7734ac1e8c90cd1e8d423a8739b3
quinceyfreeman <quinceyfreeman@outlook.com>
2021-01-10 00:17:27 -0800

    Working solution

--- calculator.py
+++ calculator.py
@@ -1,9 +1,11 @@
 def add(number_string):
     sum = 0
-    digits = number_string.split(',')
-    for digit in digits:
-        try:
-            sum += int(digit)
-        except ValueError:
-            return 0
+    lines = number_string.splitlines()
+    for line in lines:
+        digits = line.split(',')
+        for digit in digits:
+            try:
+                sum += int(digit)
+            except ValueError:
+                return 0
     return sum
--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,9 @@ def test_two_digits_seperated_by_commas():
 
 def test_three_digits_seperated():
     assert add('42,-1,17') == 58
+
+def test_two_digits_on_seperate_lines():
+    assert add('17\n42') == 59
+
+def test_multi_line_with_commas():
+    assert add('1,2\n3,4\n42') == 52
